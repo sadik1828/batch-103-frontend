d65b5034d16e1f71599c336f76a8d6af3e033dc7
sadik1828 <cumar1828@gmail.com>
2022-07-23 06:56:27 -0700

    updated project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
+import Header from "./Components/Header";
+import Restaurant from "./Pages/Restaurant";
+import Order from "./Pages/Order";
+import List from "./Pages/List";
+import RestaurantDetails from "./Pages/Admin/RestaurantDetails";
+import OrderList from "./Pages/Admin/Order";
+import Menu from "./Pages/Admin/Menu";
+import MenuForm from "./Pages/Admin/MenuForm";
+import Profile from "./Pages/Admin/Profile";
+import NotFound from "./Pages/NotFound";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurant" element={<Restaurant />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/list" element={<List />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/restaurant" element={<RestaurantDetails />} />
+        <Route path="/admin/order" element={<OrderList />} />
+        <Route path="/admin/menu/new" element={<MenuForm />} />
+        <Route path="/admin/menu" element={<Menu />} />
+        <Route path="/admin/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </BrowserRouter>
   );
 }
 
